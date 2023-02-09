14774c55bac89d0ebb9a50de06bf036256d03095
Cristian  Alb <cristian.alb@techquarter.io>
2023-02-09 15:30:04 +0200

    WIP

--- products-api/src/utils.js
+++ products-api/src/utils.js
@@ -1,7 +1,5 @@
 //Here we mock our data
 
-const loremIpsum = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-
 const products = [
     {
         productId: 1,
